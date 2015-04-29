1df578532018b5377468e291f970cc53ea5b0a9b
Mike Trentman <mike.trentman@trivantis.com>
2015-04-29 16:44:01 -0400

    first commit

---
+++ src/main/java/com/mtlogic/vboard/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mtlogic.vboard;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VBoardApplication.class);
+	}
+
+}
